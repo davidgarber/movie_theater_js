2900b9255151ccd165012cb91af76a2e78ca6d54
David Garber & Leon Zagorouiko <student@epicodus.com>
2015-06-02 14:40:57 -0700

    charges for first release and based on time.

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,22 @@ var Ticket = {
     } else if ( age <= 59) {
       return this.cost;
     }
+  },
+
+  firstRelease: function(firstRelease) {
+    if (firstRelease == 1) {
+      return this.cost;
+    } else if (firstRelease == 2) {
+      this.cost = this.cost + 3;
+      return this.cost;
+    }
+  },
+
+  movieTime: function(movieTime) {
+    if (movieTime < 6) {
+      this.cost = this.cost - 3;
+    } else if (movieTime >= 6) {
+      return this.cost;
+    }
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,10 +6,38 @@ describe("Ticket", function() {
     expect(testTicket.cost).to.equal(7)
   });
 
-  it("does not give discount", function() {
-    var testTicket = Object.create(Ticket)
-    testTicket.age(20)
-    expect(testTicket.cost).to.equal(10)
-  })
+    it("does not give discount", function() {
+      var testTicket = Object.create(Ticket)
+      testTicket.age(20)
+      expect(testTicket.cost).to.equal(10)
+  });
+});
+
+ describe("firstRelease", function() {
+   it("does not give a discount if it is not a first release", function() {
+     var testTicket = Object.create(Ticket)
+     testTicket.firstRelease(1)
+     expect(testTicket.cost).to.equal(10)
+   });
+
+   it("increases cost if it is a first release", function() {
+     var testTicket = Object.create(Ticket)
+     testTicket.firstRelease(2)
+     expect(testTicket.cost).to.equal(13)
+   });
+ });
+
+ describe("movieTime", function() {
+   it("gives a discount if time is before 6", function() {
+     var testTicket = Object.create(Ticket)
+     testTicket.movieTime(5)
+     expect(testTicket.cost).to.equal(7)
+   });
+
+   it("does not give a discount if the time is 6 or after", function() {
+     var testTicket = Object.create(Ticket)
+     testTicket.movieTime(7)
+     expect(testTicket.cost).to.equal(10)
+   });
  });
 });
